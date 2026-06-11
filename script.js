b44bee78c8e6d137ecb9dbdf068c619cc2c78571
manvigargmanu-gif <manvigargmanu@gmail.com>
2026-06-11 11:38:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.getElementById("forgiveBtn").addEventListener("click", () => {
+function scrollToLetter(){
+document.getElementById("letter").scrollIntoView({
+behavior:"smooth"
+});
+}
 
-    document.getElementById("message").innerHTML =
-    "🥹 Thank you for giving me another chance. You're genuinely important to me ❤️";
+document.getElementById("forgiveBtn")
+.addEventListener("click",function(){
 
-    confetti({
-        particleCount: 150,
-        spread: 100
-    });
+confetti({
+particleCount:150,
+spread:100,
+origin:{y:0.6}
+});
 
+document.getElementById("message").innerHTML=
+"🥹 Thank you for reading this. Every metro ride and every conversation means a lot to me. ❤️";
 });
